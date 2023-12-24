72367a50ddeef85a6de19396de6b2fcd91de7dec
edos10 <riabov.edik201533@gmail.com>
2023-12-24 11:14:42 +0300

    fix read .env

--- driver_service/internal/config/config.go
+++ driver_service/internal/config/config.go
@@ -2,9 +2,11 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 
-	"gopkg.in/yaml.v3"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -16,20 +18,27 @@ type Http struct {
 	Port int `yaml:"port"`
 }
 
-func NewConfig(filePath string) (*Config, error) {
-	curDir, _ := os.Getwd()
-	filePath = curDir + filePath
-	data, err := os.ReadFile(filePath)
+func NewConfig() (*Config, error) {
+	err := godotenv.Load()
 	if err != nil {
+		log.Fatal("Error loading .env file")
 		return nil, err
 	}
 
 	var cfg Config
-	err = yaml.Unmarshal(data, &cfg)
-	fmt.Println(cfg)
-	if err != nil {
-		return nil, err
+
+	portStr := os.Getenv("HTTP_PORT")
+	if portStr == "" {
+		return nil, fmt.Errorf("HTTP_PORT is not set in .env")
 	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return nil, fmt.Errorf("HTTP_PORT must be a valid integer")
+	}
+
+	cfg.Http.Port = port
+	cfg.SemVer = os.Getenv("SEMVER")
 
 	return &cfg, nil
 }
